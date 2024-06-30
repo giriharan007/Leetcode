e2d859a5a24029c9d9c134bda864d9f8ec51d310
Giriharan S <147045259+giriharan007@users.noreply.github.com>
2024-06-30 08:13:13 +0000

    [LeetCode Sync] Runtime - 19 ms (35.04%), Memory - 11.5 MB (88.93%)

---
+++ problems/0027-remove-element/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeElement(self,nums, val):
+        count = i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                nums.pop(i)
+                count += 1
+                continue
+            i += 1
+        return len(nums)
+        
+   
+
