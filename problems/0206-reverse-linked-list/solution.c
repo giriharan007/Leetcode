8cafe9f61066f9e512eff05db9373b937b5ae754
Giriharan S <147045259+giriharan007@users.noreply.github.com>
2024-08-26 04:01:15 +0000

    [LeetCode Sync] Runtime - 7 ms (1.37%), Memory - 10.7 MB (30.52%)

--- problems/0206-reverse-linked-list/solution.c
+++ problems/0206-reverse-linked-list/solution.c
@@ -1,15 +1,15 @@
 struct ListNode* reverseList(struct ListNode* head) {
-    struct ListNode* prev = NULL;
-    struct ListNode* curr = head;
-    struct ListNode* next = NULL;
-
-    while (curr != NULL) {
-        next = curr->next;  // Store the next node
-        curr->next = prev;  // Reverse the current node's pointer
-        prev = curr;        // Move prev to this node
-        curr = next;        // Move to the next node in the original list
+    struct ListNode* temp1 = NULL;
+    struct ListNode* temp2 = NULL;
+    while(head!=NULL){
+        temp2=head->next;
+        head->next=temp1;
+        temp1=head;
+        head=temp2;
     }
+    head=temp1;
+    return head; 
 
-    return prev;  
+    
 }
 
