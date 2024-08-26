0e99a71c525e903a3871e8b5cef180d605a91c2d
Giriharan S <147045259+giriharan007@users.noreply.github.com>
2024-08-26 04:39:10 +0000

    [LeetCode Sync] Runtime - 19 ms (1.96%), Memory - 11.5 MB (100.00%)

--- problems/0027-remove-element/solution.py
+++ problems/0027-remove-element/solution.py
@@ -1,13 +1,11 @@
 class Solution(object):
-    def removeElement(self,nums, val):
-        count = i = 0
-        while i < len(nums):
-            if nums[i] == val:
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        for i in range(len(nums)-1,-1,-1):
+            if(nums[i]==val):
                 nums.pop(i)
-                count += 1
-                continue
-            i += 1
-        return len(nums)
-        
-   
-
+        return len(nums)        
