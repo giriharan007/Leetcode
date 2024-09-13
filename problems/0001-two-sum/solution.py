cfc38f2fb8825647a57b28fafbcbf30f72685ac3
Giriharan S <147045259+giriharan007@users.noreply.github.com>
2024-09-13 12:12:40 +0000

    [LeetCode Sync] Runtime - 2205 ms (15.17%), Memory - 12.5 MB (99.94%)

--- problems/0001-two-sum/solution.py
+++ problems/0001-two-sum/solution.py
@@ -2,7 +2,7 @@ class Solution(object):
     def twoSum(self, nums, target):
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
-                if nums[j]==target-nums[i]:
+                if nums[j]+nums[i]==target:
                     return[i,j]
 
           
