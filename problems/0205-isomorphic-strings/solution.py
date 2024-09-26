74ec70f4455d997b7c4a172d7155ef1ad39cd5ea
Giriharan S <147045259+giriharan007@users.noreply.github.com>
2024-09-26 12:09:47 +0000

    [LeetCode Sync] Runtime - 24 ms (9.74%), Memory - 12.8 MB (93.36%)

--- problems/0205-isomorphic-strings/solution.py
+++ problems/0205-isomorphic-strings/solution.py
@@ -5,12 +5,27 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        str1=[]
-        str2=[]
-        for i in s:
-           str1.append(s.index(i))
-        for j in t:
-            str2.append(t.index(j))
-        if(str1==str2):
-            return True            
-        return False    
+        # str1=[]
+        # str2=[]
+        # for i in s:
+        #    str1.append(s.index(i))
+        # for j in t:
+        #     str2.append(t.index(j))
+        # if(str1==str2):
+        #     return True            
+        # return False    
+
+        if len(s)!=len(t):
+            return False
+
+        s_d={}
+        t_d={}
+
+        for i in range(len(s)):
+            if s[i] not in s_d and t[i] not in t_d:
+                s_d[s[i]]=t[i]
+                t_d[t[i]]=s[i]
+
+            elif s_d.get(s[i])!=t[i] and t_d.get(t[i])!=s[i]:
+                return False
+        return True               
