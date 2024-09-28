086c0d1060f8ad9e406ccb817197904bde14cedc
Giriharan S <147045259+giriharan007@users.noreply.github.com>
2024-09-28 04:10:54 +0000

    [LeetCode Sync] Runtime - 695 ms (5.00%), Memory - 12.9 MB (100.00%)

---
+++ problems/0979-di-string-match/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def diStringMatch(self, s):
+        I=0
+        D=len(s)
+        perm=[]
+        for i in range(0,len(s)):
+            if(s[i]=="I"):
+                perm.append(I)
+                I+=1
+            if(s[i]=="D"):
+                perm.append(D)
+                D-=1
+        for i in range(D+1):
+            if i not in perm:
+                perm.append(i)
+        return perm            
+        
