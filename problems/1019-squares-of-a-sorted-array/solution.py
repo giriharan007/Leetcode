819a1ce981acef256892de1698231ee2a35fdb9b
Giriharan S <147045259+giriharan007@users.noreply.github.com>
2024-09-29 12:00:29 +0000

    [LeetCode Sync] Runtime - 149 ms (5.05%), Memory - 13.1 MB (100.00%)

---
+++ problems/1019-squares-of-a-sorted-array/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sortedSquares(self, nums):
+        for i in range(len(nums)):
+            nums[i]=nums[i]*nums[i]
+        nums.sort()
+        return nums
+        
+  
+
+
+        
