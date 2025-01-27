4deab5573ec178c5803162ed7c933577cae5e1e4
Giriharan S <147045259+giriharan007@users.noreply.github.com>
2025-01-27 14:52:00 +0000

    [LeetCode Sync] Runtime - 5 ms (17.01%), Memory - 43.2 MB (91.70%)

---
+++ problems/0496-next-greater-element-i/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] arr=new int[nums1.length];
+
+        for(int i=0;i<nums1.length;i++){
+            int index=-1,j=nums2.length-1;
+            while(j>=0 && nums2[j] != nums1[i]){
+                if(nums2[j]>nums1[i]){
+                    index=nums2[j];
+                }
+                j--;
+            }
+            arr[i]=index;
+        }
+        return arr;
+    }
+}
