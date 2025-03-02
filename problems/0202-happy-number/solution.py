56e8b15e43e6db4f38228e72e6ac163804d6d008
Giriharan S <147045259+giriharan007@users.noreply.github.com>
2025-03-02 10:35:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.5 MB (11.22%)

--- problems/0202-happy-number/solution.py
+++ problems/0202-happy-number/solution.py
@@ -6,14 +6,11 @@ class Solution(object):
             if n in seen: 
                 return False
             seen.add(n)
-            a=[]
             sum=0
             while(n!=0):
                 rem=n%10
-                a.append(rem)
                 n=n//10
-            for i in a :
-              sum=sum+i**2
+                sum=sum+rem**2
             if(sum==1):
                 return True
             else:
