5ce00fa5e6df169725e22d9fb507bf642cc6b41a
Giriharan S <147045259+giriharan007@users.noreply.github.com>
2025-04-21 06:13:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.3 MB (11.62%)

--- problems/0101-symmetric-tree/solution.java
+++ problems/0101-symmetric-tree/solution.java
@@ -7,7 +7,7 @@ class Solution {
             return true;
         }
 
-        Queue<TreeNode> q = new LinkedList<>();
+   /*     Queue<TreeNode> q = new LinkedList<>();
         q.add(root.left);  
         q.add(root.right); 
 
@@ -32,7 +32,16 @@ class Solution {
             q.add(right.left);
         }
 
-        return true;
-    }
+        return true;*/
+    return isMirror(root.left, root.right);
+}
+
+private boolean isMirror(TreeNode t1, TreeNode t2) {
+    if (t1 == null && t2 == null) return true;
+    if (t1 == null || t2 == null) return false;
+    return (t1.val == t2.val)
+        && isMirror(t1.left, t2.right)
+        && isMirror(t1.right, t2.left);
+}
 }
 
