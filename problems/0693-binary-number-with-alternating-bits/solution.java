9c7bd4d373cd273e760cf274d7fd15916a90f663
Giriharan S <147045259+giriharan007@users.noreply.github.com>
2025-07-26 09:32:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.5 MB (69.90%)

---
+++ problems/0693-binary-number-with-alternating-bits/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int prev=n&1;
+        n=n>>1;
+        while(n!=0){
+            int curr=n&1;
+            if(curr==prev) return false;
+            n=n>>1;
+            prev=curr; 
+        }
+        return true;
+    }
+}
